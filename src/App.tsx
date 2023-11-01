d771db30d131835ac64c4eacd3c774ef2c28f765
Waydj <farhminait@gmail.com>
2023-11-01 10:44:24 +0200

    add user image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Header } from "./components/Header";
+import { ProfilePic } from "./components/ProfilePic";
 import { SideBar } from "./components/SideBar";
 
 function App() {
@@ -7,6 +8,11 @@ function App() {
     <>
       <Header />
       <SideBar />
+
+      <div className="ml-[90px] grid grid-cols-1 md:grid-cols-3">
+        <div className="col-span-2">user data</div>
+        <div className="hidden md:block"><ProfilePic/></div>
+      </div>
     </>
   );
 }
---
+++ src/components/ProfilePic.tsx
@@ -0,0 +1,8 @@
+
+export const ProfilePic = () => {
+  return (
+    <div className="fixed">
+      <img src="/user-image.png" alt="user" className="h-screen object-cover" />
+    </div>
+  )
+}
